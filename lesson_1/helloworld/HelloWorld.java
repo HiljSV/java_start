402e001bd94f43e7b251024223e0765893b186fe
Sergii <hilj7777@gmail.com>
2018-04-20 05:05:23 +0300

    Add files via upload

---
+++ lesson_1/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Task 1 (HelloWorld)
+ *
+ *  @author Khilchenko Sergii
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
